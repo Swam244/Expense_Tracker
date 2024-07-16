363f0fa99ee7d08e2697964d664dae4d68a79fa8
Swam244 <123237756+Swam244@users.noreply.github.com>
2024-07-16 23:59:48 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
+if ! command -v pip &> /dev/null
+then
+    echo "pip could not be found. Installing pip..."
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py --user
+    export PATH=$PATH:~/.local/bin
+fi
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
